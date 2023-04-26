485bae7ea30aefdfd0407346a0e6c93572898e00
archanadwivedee1234 <archanabhu1234@gmail.com>
2023-04-26 12:01:40 +0530

    commit

---
+++ Integer.py
@@ -0,0 +1,15 @@
+# Integer:
+# a data type which  contain 0-9 values only.
+
+
+a=10
+b=20
+c=a+b
+print(c)
+
+#Constructor of integer int()
+a=int(a)
+print(a)
+
+b=0 #create empty nteger using lteral
+print(b)
